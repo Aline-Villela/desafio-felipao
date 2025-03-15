1cdd3fdda15cc70a6c567d15bbff29e6cad50a4c
Aline-Villela <alinevillela93@hotmail.com>
2025-03-15 20:19:04 -0300

    Feito ajuste do switch

---
+++ index.js
@@ -0,0 +1,38 @@
+//Desafio Classificador
+
+let nomeDoHeroi = 'crash'
+let xpDoHeroi = 3000
+;
+
+switch (true) { 
+    case xpDoHeroi <= 1000: 
+    console.log("classificação do herói é de ferro");
+    break;
+     case xpDoHeroi <=2000:
+     console.log("classificação do herói é de Bronze"); 
+     break;
+    case xpDoHeroi <=5000: 
+    console.log("classificação do herói é de Prata");
+     break; 
+     case xpDoHeroi <= 6000: 
+     console.log("classificação do herói é de Ouro"); 
+     break; 
+     case xpDoHeroi <= 8000: 
+     console.log("classificação do herói é de Platina");
+      break; 
+      case xpDoHeroi <= 9000: 
+      console.log("classificação do herói é de Ascendente"); 
+      break; 
+      case xpDoHeroi <= 10000: 
+      console.log("classificação do herói é de Imortal"); 
+      break; 
+      case xpDoHeroi >= 10001: 
+    console.log("classificação do herói é Radiante"); 
+     
+    console.log(xpDoHeroi + nomeDoHeroi)
+}
+
+
+
+
+
