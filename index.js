546abeafd3e18bd9a1595338134d481d98ebabfd
Aline-Villela <alinevillela93@hotmail.com>
2025-03-25 22:47:52 -0300

    concatenar mensagem final

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 //Desafio Classificador
 
 let nomeDoHeroi = 'crash'
-let xpDoHeroi = 3000
-;
+let xpDoHeroi = 1500
+
 
 switch (true) { 
     case xpDoHeroi <= 1000: 
-    console.log("classificação do herói é de ferro");
+    console.log("o herói "  + nomeDoHeroi + " está no nível Ferro");
     break;
      case xpDoHeroi <=2000:
-     console.log("classificação do herói é de Bronze"); 
+     console.log("o herói "  + nomeDoHeroi + " está no nível Bronze"); 
      break;
     case xpDoHeroi <=5000: 
-    console.log("classificação do herói é de Prata");
+    console.log("o herói "  + nomeDoHeroi + " está no nível Prata");
      break; 
      case xpDoHeroi <= 6000: 
-     console.log("classificação do herói é de Ouro"); 
+     console.log("o herói "  + nomeDoHeroi + " está no nível Ouro"); 
      break; 
      case xpDoHeroi <= 8000: 
-     console.log("classificação do herói é de Platina");
+     console.log("o herói "  + nomeDoHeroi + " está no nível Platina");
       break; 
       case xpDoHeroi <= 9000: 
-      console.log("classificação do herói é de Ascendente"); 
+      console.log("o herói "  + nomeDoHeroi + " está no nível Ascendente"); 
       break; 
       case xpDoHeroi <= 10000: 
-      console.log("classificação do herói é de Imortal"); 
+      console.log("o herói "  + nomeDoHeroi + " está no nível Imortal"); 
       break; 
       case xpDoHeroi >= 10001: 
-    console.log("classificação do herói é Radiante"); 
+    console.log("o herói "  + nomeDoHeroi + " está no nível Radiante"); 
     break;
 
 }
     
-    console.log( "o herói " + nomeDoHeroi  +  " está no nivel "  +  xpDoHeroi )
+    
 
 
 
