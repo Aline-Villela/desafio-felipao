409ffec6da54292f205b043c22e16507044dcbd4
Aline-Villela <alinevillela93@hotmail.com>
2025-04-07 18:25:45 -0300

    Adiciona desafio2 com níveis de herói

---
+++ desafio2.js
@@ -0,0 +1,36 @@
+let total = saldoDeVitorias(86,37)
+
+
+console.log (`o saldo de vitórias do herói é ${total}`)
+
+function saldoDeVitorias(vitorias, derrotas){
+ let resultado = vitorias - derrotas 
+ return resultado 
+}
+ 
+ let nivelHeroi = 65
+ 
+switch (true){
+    case nivelHeroi <= 10: 
+    console.log("o herói está no nível Ferro");
+    break;
+     case nivelHeroi <=20:
+    console.log("o herói está no nível Bronze"); 
+     break;
+    case nivelHeroi <=50: 
+    console.log("o herói está no nível Prata");
+     break; 
+     case nivelHeroi <= 80: 
+     console.log("o herói está no nível Ouro"); 
+     break; 
+     case nivelHeroi <= 90: 
+     console.log("o herói está no nível Diamante");
+      break; 
+      case nivelHeroi <= 100: 
+      console.log("o herói está no nível lendário"); 
+      break; 
+     case nivelHeroi >= 101: 
+     console.log("o herói está no nível Radiante"); 
+    break;
+
+}
--- index.js
+++ index.js
@@ -31,6 +31,7 @@ switch (true) {
     break;
 
 }
+
     
     
 
